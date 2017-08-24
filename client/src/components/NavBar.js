472b257ea8cdc40007f77984aec53607f0089191
Michael Demerjian <michaeldemerjian@gmail.com>
2017-08-24 16:12:05 -0700

    eeeek

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,11 +6,12 @@ import { logout } from '../actions/user';
 
 const links = [
   { name: 'Home', path: '/' },
-  { name: 'Timeclocks', path: '/about'},
+  
 ]
 
 const authenticatedLinks = [
   { name: 'Dashboard', path: '/dashboard' },
+  { name: 'Timeclocks', path: '/about'},
   { name: 'Logout' }
 ]
 
